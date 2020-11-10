39a97c3936b997a01af1ec0245c6dbbafdcb3425
saltemose <sean@seanaltemose.com>
2020-11-09 20:20:26 -0500

    Require auth before entry and update auth views with Sign In or Create Account links

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class TasksController < ApplicationController
+  before_action :require_current_user!, except: [:create, :new]
   before_action :set_task, only: %i[show edit update destroy]
 
   # GET /tasks
